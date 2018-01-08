c7431a1e084ca10b1abcca538181840943945260
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-01-08 07:33:35 +0530

    Updated basic layout of the app
    removed the floating action button and updated the toolbar

--- app/src/main/java/com/example/dipansh/ongaku_android/MainActivity.java
+++ app/src/main/java/com/example/dipansh/ongaku_android/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     @Override
